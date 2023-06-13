82040de1d0268fcd9ed8dcfbdb03eda2b38aa6bd
Carla <113267384+RuixDsgn@users.noreply.github.com>
2023-06-13 13:46:13 -0700

    complete python lab with all tests passing

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    countdown = 10
+
+    while countdown > 0:
+        print(countdown)
+        countdown -= 1
+
+    print("Happy New Year!")
+    
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_numbers = [num * num for num in int_list]
+    return squared_numbers
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for countdown in range(1, 101):
+        if countdown % 5 == 0 and countdown % 3 == 0:
+            print ("FizzBuzz")
+        elif countdown % 5 == 0:
+            print ("Buzz")
+        elif countdown % 3 == 0:
+            print ("Fizz")
+        else:
+            print (countdown)
+        
+        
